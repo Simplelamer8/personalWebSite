7a841d29624611f5fcc46863dbc5665739cd0e7f
Simplelamer8 <67641980+Simplelamer8@users.noreply.github.com>
2024-04-07 10:55:29 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/*.{html,jsx}",
+    "./src/*.{html,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
