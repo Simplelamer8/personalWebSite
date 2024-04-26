3bb1fbd8c427ac0fc7df66eb45740bcf7d10d182
Simplelamer8 <67641980+Simplelamer8@users.noreply.github.com>
2024-04-26 19:46:16 +0500

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -2,6 +2,7 @@ import React, {Suspense} from 'react'
 import {Canvas} from "@react-three/fiber";
 import {Decal, Float, OrbitControls, Preload, useTexture} from "@react-three/drei";
 import CanvasLoader from "../Loader";
+import { geometry } from 'maath';
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -10,9 +11,10 @@ const Ball = (props) => {
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]}/>
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]}/>
+        {/* <icosahedronGeometry args={[1, 1]}/> */}
+        <boxGeometry/>
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading/>
-        <Decal map={decal} position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} flatShading />
+        <Decal map={decal} position={[0, 0, 0.5]} rotation={[2 * Math.PI, 0, 6.25]} flatShading />
       </mesh>
     </Float>
   )
